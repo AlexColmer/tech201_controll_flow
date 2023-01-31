3d020bbf4b98be0fb9788b4f23463c8477262ea1
Alex Colmer <alexander.colmer@gmail.com>
2023-01-31 11:49:42 +0000

    added if, elif, and else statments

--- if_statements.py
+++ if_statements.py
@@ -1,15 +1,11 @@
-# Control flow
-flow through a particular decision process 
+# age = 15
+#
+# if age >= 18:
+#     print("Your are the correct age to watch this film and can buy a ticket")
+# elif age < 18:
+#     print("I'm afraid you cannot watch this movie, you are not old enough")
 
-all about making decisions in the program 
-
-### if statements 
-use if, elif and else to write a program
-
-simple film rating program
-
-``` 
-film_rating = "universal"
+film_rating = "12"
 
 if film_rating.lower() == "universal":
     print("All age groups can watch this film")
@@ -20,6 +16,6 @@ elif film_rating.lower() == "12" or film_rating == "12a":
 elif film_rating.lower() == "15":
     print("You must be 15 to watch 15 rated movies in the cinema")
 elif film_rating.lower() == "18":
-    print("You must be 18 to watch 18 movies in the cinema") 
-```
-there are no 'switch statements' or 'case statements' used in other languages
+    print("You must be 18 to watch 18 movies in the cinema")
+else:
+    print("This is not a correct rating, please use universal, pg, 12 or 12a, 15, 18")
