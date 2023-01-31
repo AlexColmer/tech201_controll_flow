c28c670faa13784b091d4e4f41ed836bad9d8e6b
Alex Colmer <alexander.colmer@gmail.com>
2023-01-31 17:19:34 +0000

    completed task1 and 2

--- New_film_rating_task.py
+++ New_film_rating_task.py
@@ -1,13 +1,19 @@
-age = input("What is your age? ")
+user_prompt = True
 
-if age.isdigit() and int(age) > 0 and int (age) < 117:
+age = input("What is your age? ")
+if age.isdigit() and int(age) < 117 and int(age) > 1:
+    user_prompt = False
+else:
+    print("the value you have entered is invalid pleas enter a digit between 0 - 117 for example 12")
+
+if age.isdigit() and 0 < int(age) < 117:
     if int(age) >= 18:
         print("You can watch all films")
     elif int(age) < 18 and int(age) >= 15:
         print("You can watch u, pg, 12, 12a and 15")
     elif int(age) < 15 and int(age) >= 12:
-        print("You can watch u, pg 12 adn 12a")
-    else:
+        print("You can watch u, pg 12 and 12a")
+    elif int(age) < 12:
         print("You can only watch u and pg films")
-else:
-    ("the value you have enterd is invlaid pleas enter a digit for example 12")
+
+
