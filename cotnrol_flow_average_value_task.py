12a4cc6defb056c4da54e434099ef814802dc9ac
Alex Colmer <alexander.colmer@gmail.com>
2023-01-31 17:56:52 +0000

    added average valuen task

---
+++ cotnrol_flow_average_value_task.py
@@ -0,0 +1,18 @@
+num1 = int(input("enter the 1st number: "))
+num2 = int(input("enter the 2nd number: "))
+num3 = int(input("enter the 3th number: "))
+num4 = int(input("enter the 4th number: "))
+num5 = int(input("enter the 5th number: "))
+num6 = int(input("enter the 6th number: "))
+num7 = int(input("enter the 7th number: "))
+num8 = int(input("enter the 8th number: "))
+num9 = int(input("enter the 9th number: "))
+num10 = int(input("enter the 10th number: "))
+average = (num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10) / 10
+
+print("The average is: ", round(average))
+
+counter = 0
+while counter <= 300:
+    print(counter)
+    counter += 10
